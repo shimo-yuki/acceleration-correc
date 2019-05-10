96195223ebaebe695f776d52fd642777a50c15bc
shimo_yuki <c0116138b7@edu.teu.ac.jp>
2019-05-10 15:46:55 +0900

    5秒ごとにグラフを更新するようにした

--- index.js
+++ index.js
@@ -2,20 +2,13 @@
 
 var aX = 0, aY = 0, aZ = 0;
 var gX = [], gY = [], gZ = [];
+var z = [];
 window.addEventListener("devicemotion", (dat) => {
     aX = dat.accelerationIncludingGravity.x;
     aY = dat.accelerationIncludingGravity.y;
     aZ = dat.accelerationIncludingGravity.z;
 });
 
-var timer = window.setInterval(() =>{
-    displayData();
-    gX.push(aX);
-    gY.push(aY);
-    gZ.push(aZ);
-}, 10);
-
-console.log(gX);
 function displayData(){
     var txt = document.getElementById("txt");
     txt.innerHTML = "x: " + aX + "<br>"
@@ -23,8 +16,26 @@ function displayData(){
                     + "z: " + aZ;
 }
 
+var timer = window.setInterval(() =>{
+    displayData();
+}, 10);
+
+var count = 0;
+var id = setInterval(function(){
+    count++;
+    console.log(count);
+    gX.push(aX);
+    gY.push(aY);
+    gZ.push(aZ);
+    if(count % 5 == 0){　
+      displayGraph();
+    }}, 1000);
+
+
 /////////////////////////////////////////////////////////
-window.onload = function() {
+/////////////////////////表示系///////////////////////////
+function displayGraph(){
+    console.log('aa');
     ctx = document.getElementById("canvas").getContext("2d");
     window.myBar = new Chart(ctx, {
         type: 'line', 
@@ -43,7 +54,7 @@ var barChartData = {
     {
         label: 'y1',
         fill: false,
-        data: gX,
+        data: z,
         borderColor : "rgba(254,97,132,0.8)"
     },
     {
@@ -60,40 +71,4 @@ var barChartData = {
     },
     ],
 };
-
-// var ctx = document.getElementById("chart");
-// var chartData  = {
-//     labels: ['1', '2', '3', '4', '5'],
-//     datasets: [
-//         {
-//             type: 'line',
-//             label: 'sample-line',
-//             data: ['0.155','0.118','0.121','0.068','0.083','0.060','0.067',
-//             '0.121','0.121','0.150','0.118','0.097','0.078','0.127',
-//             '0.155','0.140','0.101','0.140','0.041','0.093','0.189',
-//             '0.146','0.134','0.127','0.116','0.111','0.125','0.116'
-//             ],
-//             borderColor : "rgba(254,97,132,0.8)",
-//             backgroundColor : "rgba(254,97,132,0.5)",
-//         },
-//         {
-//         type: 'line',
-//         label: 'sample-line',
-//         data: ['0.3','0.1','0.1','0.3','0.4','0.2','0.0',
-//             '0.2','0.3','0.11','0.5','0.2','0.5','0.4',
-//             '0.0','0.3','0.7','0.3','0.6','0.4','0.9',
-//             '0.7','0.4','0.8','0.7','0.4','0.7','0.8'
-//         ],
-//         borderColor : "rgba(54,164,235,0.8)",
-//         backgroundColor : "rgba(54,164,235,0.5)",
-//         },
-//     ],
-// };
-
-// var chart = new CanvasJS.Chart("chart",{
-//     type: 'bar',
-//     data: chartData
-
-// });
-
-// chart.render();
+/////////////////////////////////////////////////////////
