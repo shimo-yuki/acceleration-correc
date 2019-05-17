c481605e43140a0564147539f97a9a23c2ccb2a5
shimo_yuki <c0116138b7@edu.teu.ac.jp>
2019-05-17 13:35:20 +0900

    display x,y,z

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 ////////ß////////////加速度について//////////////////////////
 
 var aX = 0, aY = 0, aZ = 0;
-var gX = [], gY = [], gZ = [];
-// var gX1 = [], gX2 = [], gX3 = [], gX4 = [], gX5 = []
-// var gY1 = [], gY2 = [], gY3 = [], gY4 = [], gY5 = [];
-// var gZ1 = [], gZ2 = [], gZ3 = [], gZ4 = [], gZ5 = [];
 
 window.addEventListener("devicemotion", (dat) => {
     aX = dat.accelerationIncludingGravity.x;
@@ -20,37 +16,8 @@ function displayData(){
 }
 
 
-
-/////////////////////////////////////////////////////////
-/////////////////////////表示系///////////////////////////
-//やること→動的にデータが後ろから前に変わるようにする
-// .shiftで先頭を取る
-// .pushで追加する
-var xAxis = 5;
-var labels = [];
-for(var i = 0; i < xAxis; i++){
-    labels.push(i);
-}
-var count = 0;
-var id = setInterval(function(){
-    count++;
-
-    gX.push(aX);
-    gY.push(aY);
-    gZ.push(aZ);
-    if(count % 5 == 0){　
-    //   displayGraph();
-      gX = [], gY = [], gZ = [];
-    }}, 1000);
-
-// function displayGraph(){
-    var tx = document.getElementById("tx");
-    tx.innerHTML = "x: " + gX + "<br>"
-                    + "y: " + gY + "<br>"
-                    + "z: " + gZ;
-                    console.log(gX);
-
     ctx = document.getElementById("canvas").getContext("2d");
+    displayData();
     window.myBar = new Chart(ctx, {
         type: 'line', 
         fill: false,
@@ -120,8 +87,8 @@ var id = setInterval(function(){
             }
         }
     });
-// };
-var count = 0;
+
+
 function get_data1() {
     return aX;
 }
@@ -131,27 +98,3 @@ function get_data2() {
 function get_data3() {
     return aZ;
 }
-var barChartData = {
-    labels: labels,
-    datasets: [
-    {
-        label: 'X方向',
-        fill: false,
-        data: aX,
-        borderColor : "rgba(254,97,132,0.8)"
-    },
-    {
-        label: 'Y方向',
-        fill: false,
-        data: aY,
-        borderColor : "rgba(54,164,235,0.8)"
-    },
-    {
-        label: 'Z方向',
-        fill: false,
-        data: aZ,
-        borderColor : "rgba(255,255,0,0.8)"
-    },
-    ],
-};
-/////////////////////////////////////////////////////////
