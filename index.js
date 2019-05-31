bae88a62f46e66718814070627c0631ad19573b2
shimo_yuki <c0116138b7@edu.teu.ac.jp>
2019-05-31 14:57:54 +0900

    change graph size

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ window.myBar = new Chart(ctz, {
             xAxes: [{                          
                 type: 'realtime',  
                 realtime: {         
-                    duration: 1500,    
+                    duration: 1700,    
                     refresh: 300,    
                     delay: 1000,   
                     pause: false, 
