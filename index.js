544542fe51e6abfc424f67d5473ecd2fb5eeb411
shimo-yu <yf521985@gmail.com>
2019-12-22 22:19:04 +0900

    change Z

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ window.addEventListener('devicemotion', (dat) => {
 		if (navigator.userAgent.indexOf(ua[i]) > 0) {
             aX = -dat.accelerationIncludingGravity.x;    // x軸の重力加速度（Android と iOSでは正負が逆）
             aY = -dat.accelerationIncludingGravity.y;    // y軸の重力加速度（Android と iOSでは正負が逆）
-            aZ = -dat.accelerationIncludingGravity.z;    // z軸の重力加速度（Android と iOSでは正負が逆）
+            aZ = dat.acceleration.z;    // z軸の重力加速度（Android と iOSでは正負が逆）
             break;
 		}
 	}
     aX = dat.accelerationIncludingGravity.x;    // x軸の重力加速度（Android と iOSでは正負が逆）
     aY = dat.accelerationIncludingGravity.y;    // y軸の重力加速度（Android と iOSでは正負が逆）
-    aZ = dat.accelerationIncludingGravity.z;    // z軸の重力加速度（Android と iOSでは正負が逆）
+    aZ = dat.acceleration.z;    // z軸の重力加速度（Android と iOSでは正負が逆）
 });
  
 // 指定時間ごとに繰り返し実行される setInterval(実行する内容, 間隔[ms]) タイマーを設定
